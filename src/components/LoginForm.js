b7cd2e652e8b93de89dfce9bd2eafef5362d1340
MayakaOsinde <mayakaosinde@gmail.com>
2023-01-24 15:47:22 +0300

    Installed Font Awesome dependencies. Installed state. Updated my signup form code.

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,6 +1,10 @@
 import React from "react";
+import { useState, useEffect, useRef} from "react"
+
 
 function LoginForm(){
+
+    
 
     return (
         <div>
--- src/components/SignUpForm.js
+++ src/components/SignUpForm.js
@@ -1,10 +1,26 @@
-import React from "react";
+import React, { useRef, useState } from "react";
+import { faCheck, faTimes, faInfoCircle } from "@fortawesome/free-solid-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"  
 
-function SignUpForm(){
+const USER_REGEX = /^[a-zA-Z][a-zA-Z0-9-_]{3,23}$/;
+const PWD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8,24}$/;
+
+
+
+const SignUpForm = () => {
+
+    const userRef = useRef();
+    const errRef = useRef(); 
+
+    const [user, setUser] = useState('');
+    const [validName, setValidName] = useState(false);
+    const [userFocus, setUserFocus] = useState(false);
 
     return (
         <div>
             <input type="text" placeholder="Enter Username"></input>
+            <input type="text" placeholder="Enter Email"></input>
+            <input type="text" placeholder="Enter Access Token"></input>
             <input type="text" placeholder="Enter Password"></input>
             <button type="submit">Sign Up</button>
         </div>
