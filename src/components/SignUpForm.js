8ff4c9d34c30f4221e711ede8b28f60f09459382
MayakaOsinde <mayakaosinde@gmail.com>
2023-01-24 17:13:08 +0300

    Rendered my warning message paragraph for invalid credentials. Rendered my form (Username input only)

--- src/components/SignUpForm.js
+++ src/components/SignUpForm.js
@@ -1,63 +1,83 @@
 import React, { useEffect, useRef, useState } from "react";
-import { faCheck, faTimes, faInfoCircle } from "@fortawesome/free-solid-svg-icons"
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"  
+import {
+  faCheck,
+  faTimes,
+  faInfoCircle,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const USER_REGEX = /^[a-zA-Z][a-zA-Z0-9-_]{3,23}$/;
 const PWD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8,24}$/;
 
-
-
 const SignUpForm = () => {
+  const userRef = useRef();
+  const errRef = useRef();
 
-    const userRef = useRef();
-    const errRef = useRef(); 
+  const [user, setUser] = useState("");
+  const [validName, setValidName] = useState(false);
+  const [userFocus, setUserFocus] = useState(false);
 
-    const [user, setUser] = useState('');
-    const [validName, setValidName] = useState(false);
-    const [userFocus, setUserFocus] = useState(false);
+  const [pwd, setPwd] = useState("");
+  const [validPwd, setValidPwd] = useState(false);
+  const [pwdFocus, setPwdFocus] = useState(false);
 
-    const [pwd, setPwd] = useState('');
-    const [validPwd, setValidPwd] = useState(false);
-    const [pwdFocus, setPwdFocus] = useState(false);
+  const [matchPwd, setMatchPwd] = useState("");
+  const [validMatch, setValidMatch] = useState(false);
+  const [matchFocus, setMatchFocus] = useState(false);
 
-    const [matchPwd, setMatchPwd] = useState('');
-    const [validMatch, setValidMatch] = useState(false);
-    const [matchFocus, setMatchFocus] = useState(false);
+  const [errMsg, setErrMsg] = useState("");
+  const [success, setSuccess] = useState(false);
 
-    const [errMsg, setErrMsg] = useState('');
-    const [success, setSuccess] = useState(false);
+  useEffect(() => {
+    userRef.current.focus();
+  }, []);
 
-    useEffect(() => {
-        userRef.current.focus()
-    }, [])
+  useEffect(() => {
+    const result = USER_REGEX.test(user);
+    console.log(result);
+    console.log(user);
+    setValidName(result);
+  }, [user]);
 
-    useEffect(() => {
-        const result = USER_REGEX.test(user)
-        console.log(result);
-        console.log(user);
-        setValidName(result)
-    }, [user])
+  useEffect(() => {
+    const result = PWD_REGEX.test(pwd);
+    console.log(result);
+    console.log(pwd);
+    setValidPwd(result);
+    const match = pwd === matchPwd;
+    setValidMatch(match);
+  }, [pwd, matchPwd]);
 
-    useEffect(() => {
-        const result = PWD_REGEX.test(pwd);
-        console.log(result);
-        console.log(pwd);
-        setValidPwd(result);
-        const match = pwd === matchPwd;
-        setValidMatch(match)
-    }, [pwd, ])
+  useEffect(() => {
+    setErrMsg("");
+  }, [user, pwd, matchPwd]);
 
-    
-
-    return (
-        <div>
-            <input type="text" placeholder="Enter Username"></input>
-            <input type="text" placeholder="Enter Email"></input>
-            <input type="text" placeholder="Enter Access Token"></input>
-            <input type="text" placeholder="Enter Password"></input>
-            <button type="submit">Sign Up</button>
-        </div>
-    )
-
-} 
-export default SignUpForm
+  return (
+    <section>
+      <p
+        ref={errRef}
+        className={errMsg ? "errmsg" : "offscreen"}
+        aria-live="assertive"
+      >
+        {errMsg}
+      </p>
+      <h1>Register</h1>
+      <form>
+        <label htmlFor="username">Username:</label>
+        <input
+        type="text"
+        id="username"
+        ref={userRef}
+        autoComplete="off"
+        onChange={(e) => setUser(e.target.value)}
+        required
+        aria-invalid={validName ? "false" : "true"}
+        aria-describedby="uidnote"
+        onFocus={() => setUserFocus(true)}
+        onBlur={() => setUserFocus(false)}
+        />
+      </form>
+    </section>
+  );
+};
+export default SignUpForm;
