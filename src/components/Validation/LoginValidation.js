ca80c7da7a8aaf7cc63d65a567beb7911f92d3ce
MayakaOsinde <mayakaosinde@gmail.com>
2023-01-27 08:37:51 +0300

    Created components each with a full sign up and login page to be routed to.

---
+++ src/components/Validation/LoginValidation.js
@@ -0,0 +1,14 @@
+import React from "react";
+import LoginForm from "./LoginForm";
+import NavBar from "./NavBar";
+
+function LoginValidation(){
+
+    return(
+        <div>
+            <NavBar/>
+            <LoginForm/>
+        </div>
+    )
+}
+export default LoginValidation
---
+++ src/components/Validation/SignUpValidation.js
@@ -0,0 +1,12 @@
+import React from "react";
+import LoginForm from "./LoginForm";
+import NavBar from "./NavBar";
+
+function SignUpValidation(){
+    return(
+        <div>
+            <NavBar/>
+            <LoginForm/>
+        </div>
+    )
+}
