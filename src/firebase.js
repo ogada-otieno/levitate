dc89c564b0e51170b0d97d073e78ced06577b893
Rodgers Ogada <ogadarodgers@gmail.com>
2023-02-01 10:44:11 +0300

    Imported realtime database from firebase server

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import {getAuth} from "firebase/auth";
+import { getAuth } from "firebase/auth";
+import { getDatabase } from "firebase/database";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -11,7 +12,7 @@ const firebaseConfig = {
   projectId: "levitate-bef0d",
   storageBucket: "levitate-bef0d.appspot.com",
   messagingSenderId: "1061039372239",
-  appId: "1:1061039372239:web:14b4f809a76ab519ce84ba"
+  appId: "1:1061039372239:web:14b4f809a76ab519ce84ba",
 };
 
 // Initialize Firebase
@@ -20,4 +21,7 @@ const app = initializeApp(firebaseConfig);
 // Initialize Firebase Authentication and get a reference to the service
 const auth = getAuth(app);
 
-export {auth, app}
+// Initialize Realtime Database and get a reference to the service
+const database = getDatabase(app);
+
+export { auth, app, database };
