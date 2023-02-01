86eeeeda9211e1bdccac71615875789aaa78210a
Rodgers Ogada <ogadarodgers@gmail.com>
2023-02-01 10:47:46 +0300

    The Navbar is now able to display and change with the change in state of login/logout.

--- src/Pages/Navbar.js
+++ src/Pages/Navbar.js
@@ -7,32 +7,44 @@ function Navbar() {
   return (
     <nav className="ui navbar brown">
       <div>
+        <h3>
+          <Link to="/">Home</Link>
+        </h3>
         <ul className="navbar-list">
-          <li className="navbar-item">
-            <Link to="/">Home</Link>
-            &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
-            <Link to="/projects">Projects</Link>
-            &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
-            <Link to="/create">Add Project</Link>
-          </li>
+          {isAuthenticated() && (
+            <li className="navbar-item">
+              &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
+              <Link to="/projects">Projects</Link>
+              &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
+              <Link to="/create">Add Project</Link>
+            </li>
+          )}
         </ul>
       </div>
       <div className="rightitems">
         <ul className="navbar-list">
-         {!isAuthenticated() && ( <><li className="navbar-item">
-            <Link to="/login">Login</Link>
-          </li>
-          
-            <li className="navbar-item">
-              <Link to="/signup">Signup</Link>
-            </li></>
+          {!isAuthenticated() && (
+            <>
+              <li className="navbar-item">
+                <Link to="/login">Login</Link>
+              </li>
+
+              <li className="navbar-item">
+                <Link to="/signup">Signup</Link>
+              </li>
+            </>
           )}
-          <li className="navbar-item" onClick={() => Logout()}>
-            <Link to="/login">Logout</Link>
-          </li>
-          <li className="navbar-item">
-            <Link to="/profile">Profile</Link>
-          </li>
+          {isAuthenticated() && (
+            <>
+              <li className="navbar-item" onClick={() => Logout()}>
+                <Link to="/login">Logout</Link>
+              </li>
+
+              <li className="navbar-item">
+                <Link to="/profile">Profile</Link>
+              </li>
+            </>
+          )}
         </ul>
       </div>
     </nav>
